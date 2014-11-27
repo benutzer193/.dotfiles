5a91f0d7b5dbe09f8f4a4e6369991eb932b79b6f
HerrNieschnell <registerbn@gmail.com>
2014-11-27 18:37:53 +0100

    modified:   .gitignore new file:   browser/install.sh new file:   i3/install.sh new file:   mplayer/install.sh new file:   mutt/install.sh new file:   ranger/install.sh modified:   script/install new file:   zathura/install.sh modified:   zsh/config.zsh new file:   zsh/install.sh

---
+++ mplayer/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+APP=mplayer
+
+if test ! $(which $APP)
+then
+	 echo "  Installing $APP for you."
+	 $PACKMANAGER $APP > /tmp/$APP-install.log
+    fi
+
+unset APP
---
+++ mutt/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+APP=mutt
+
+if test ! $(which $APP)
+then
+	 echo "  Installing $APP for you."
+	 $PACKMANAGER $APP > /tmp/$APP-install.log
+    fi
+
+unset APP
