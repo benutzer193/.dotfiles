cb038300c77038a9182bc3fb1e44147b34db0871
HerrNieschnell <registerbn@gmail.com>
2014-11-27 23:24:14 +0100

    modified:   ../browser/install.sh modified:   ../i3/install.sh modified:   ../mplayer/install.sh modified:   ../mutt/install.sh modified:   ../ranger/install.sh modified:   install modified:   ../zathura/install.sh modified:   ../zsh/install.sh

--- i3/install.sh
+++ i3/install.sh
@@ -3,8 +3,9 @@ APP=i3
 
 if test ! $(which $APP)
 then
-	 echo "  Installing $APP for you."
-	 $PACKMANAGER $APP > /tmp/$APP-install.log
-    fi
+	 export APPS="$APPS $APP"
+	 echo -e "\033[0,31m Adding $APP to apps being installed."
+ fi
 
+export APPS="$APPS"
 unset APP
