0fb62d26f88ca051853597eeb5f8f555e35d5b8e
HerrNieschnell <registerbn@gmail.com>
2014-11-27 12:28:33 +0100

    Update alias.zsh

--- texlive/alias.zsh
+++ texlive/alias.zsh
@@ -1,24 +1,4 @@
-#blalSystem tools
-alias ls="ls --color=auto -a"
-alias mount="sudo mount"
-alias umount="sudo umount"
-alias xclip="xclip -selection c"
-alias xpaste="xclip -selection clipboard -o"
-
-# Power options 
-alias shutdown="sudo shutdown"
-alias reboot="sudo reboot"
-
-# TexLive
 alias tlin="sudo tlmgr install"
 alias tlmgr="sudo tlmgr"
 alias tlup="sudo tlmgr update --all && sudo texhash ~/.texmf && sudo texhash"
-alias tldump="rm -i ~/.dotfiles/texlivedb && tlmgr dump-tlpdb --local > ~/.dotfiles/texlivedb"
-
-# VPN / RDP
-alias vpnuni="sudo vpnc unimainz"
-alias vpns="sudo vpnc-disconnect"
-alias rbnhs="xfreerdp +clipboard /v:BNHS /u:Administrator /drive:/home/bnichell"
-
-# other
-alias svim="sudo vim"
+alias tldump="rm -i ~/.dotfiles/texlive/texlivedb && tlmgr dump-tlpdb --local > ~/.dotfiles/texlivedb"
