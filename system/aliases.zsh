4f36b2d26735be19270fd19ee8d5af87885ea04e
HerrNieschnell <registerbn@gmail.com>
2014-11-28 13:57:13 +0100

    Update aliases.zsh

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,7 +1,26 @@
-alias ls="ls --color=auto -a"
+# ls commands
+alias ls="ls -X --color=auto"
+alias la="ls -a"
+alias ll="ls -lh"
+alias l.="ls -d .*"
+
+# sudo
 alias mount="sudo mount"
 alias umount="sudo umount"
-alias xclip="xclip -selection c"
-alias xpaste="xclip -selection clipboard -o"
 alias shutdown="sudo shutdown"
 alias reboot="sudo reboot"
+
+# cd commands
+alias cd..='cd ..'
+alias ..='cd ..'
+alias ...='cd ../../'
+alias ....='cd ../../../'
+alias .....='cd ../../../../'
+alias .2='cd ../../'
+alias .3='cd ../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../../'
+
+# copy / paste
+alias xclip="xclip -selection c"
+alias xpaste="xclip -selection clipboard -o"
