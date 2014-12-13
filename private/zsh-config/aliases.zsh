33f11d29ee667a8ba2d8c2df1837fd0eb097da9e
HerrNieschnell <registerbn@gmail.com>
2014-12-13 16:27:24 +0100

    new structure new file:   git/zsh-config/aliases.zsh new file:   git/zsh-config/completion.zsh new file:   pacman/zsh-config/aliases.zsh new file:   private/zsh-config/aliases.zsh new file:   private/zsh-config/env-vars.zsh new file:   system/zsh-config/aliases.zsh new file:   system/zsh-config/env.zsh new file:   system/zsh-config/grc.zsh new file:   system/zsh-config/keys.zsh new file:   system/zsh-config/path.zsh new file:   texlive/zsh-config/aliases.zsh new file:   texlive/zsh-config/path.zsh new file:   vim/zsh-config/aliases.zsh new file:   zsh/prompt-themes/prompt-pure.zsh new file:   zsh/prompt-themes/prompt.zsh new file:   zsh/zsh-config/aliases.zsh new file:   zsh/zsh-config/cd.zsh new file:   zsh/zsh-config/compl.zsh new file:   zsh/zsh-config/completion.zsh new file:   zsh/zsh-config/config.zsh new file:   zsh/zsh-config/fpath.zsh new file:   zsh/zsh-config/history.zsh new file:   zsh/zsh-config/io.zsh new file:   zsh/zsh-config/keys new file:   zsh/zsh-config/keys.zsh new file:   zsh/zsh-config/prompt-pure.zsh new file:   zsh/zsh-config/window.zsh

---
+++ private/zsh-config/aliases.zsh
@@ -0,0 +1,4 @@
+# VPN / RDP
+alias vpnuni="sudo vpnc unimainz"
+alias vpns="sudo vpnc-disconnect"
+alias rbnhs="xfreerdp +clipboard /v:BNHS /u:Administrator /drive:/home/bnichell"
---
+++ system/zsh-config/path.zsh
@@ -0,0 +1,2 @@
+export PATH=$HOME/:./bin:bin:/usr/local/bin:$ZSH/bin:$PATH
+export MANPATH=/usr/local/man:$MANPATH:/usr/local/mysql/man:/usr/local/git/man:$MANPATH
