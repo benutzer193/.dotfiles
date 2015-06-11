3902096c27e16016e163c2e65bfc87ed549818c1
benutzer193 <registerbn@gmail.com>
2015-06-11 19:26:53 +0200

    A lot small changes

--- private/zsh-config/aliases.zsh
+++ private/zsh-config/aliases.zsh
@@ -1,5 +1,5 @@
 # VPN / RDP
 alias vpnuni="sudo vpnc unimainz"
 alias vpns="sudo vpnc-disconnect"
-alias rbnhs="xfreerdp +clipboard /v:BNHS /u:Administrator /drive:/home/bnichell"
-alias ba="vim ~/Documents/university/bachelor_thesis/tex/parts/bachelor_2.tex ~/Documents/university/bachelor_thesis/tex/bachelor_main.tex"
+alias rbnhs="xfreerdp +clipboard /t:BNHS-remote /f -grab-keyboard /v:BNHS /u:Administrator /drive:/home/bnichell"
+alias sbnhs="sudo net rpc shutdown -C 'remote shutdown from bn_archbook' -I BNHS"
