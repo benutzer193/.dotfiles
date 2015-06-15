82d4a1a532754f0545c50793805785de53734ce2
benutzer193 <registerbn@gmail.com>
2015-06-15 16:22:58 +0200

    small changes

--- private/zsh-config/aliases.zsh
+++
@@ -1,5 +0,0 @@
-# VPN / RDP
-alias vpnuni="sudo vpnc unimainz"
-alias vpns="sudo vpnc-disconnect"
-alias rbnhs="xfreerdp +clipboard /t:BNHS-remote /f -grab-keyboard /v:BNHS /u:Administrator /drive:/home/bnichell"
-alias sbnhs="sudo net rpc shutdown -C 'remote shutdown from bn_archbook' -I BNHS -U Administrator"
