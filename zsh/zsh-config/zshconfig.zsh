6da8d137b81bb33e590e87b77f1d239affe5c20e
benutzer193 <registerbn@gmail.com>
2016-07-31 20:22:55 +0200

    Merge Zsh config in one file

---
+++ zsh/zsh-config/zshconfig.zsh
@@ -0,0 +1,62 @@
+export EDITOR="vim"
+
+setopt NO_BG_NICE               # don't nice background tasks
+setopt NO_NOTIFY                # No notification  on bg-process stop
+setopt NO_HUP
+setopt NO_LIST_BEEP
+setopt LOCAL_OPTIONS            # allow functions to have local options
+setopt LOCAL_TRAPS              # allow functions to have local traps
+setopt PROMPT_SUBST
+setopt CORRECT
+
+setopt IGNORE_EOF
+setopt NO_BEEP
+
+unsetopt CLOBBER
+
+#############
+#  history  #
+#############
+
+
+HISTFILE=~/.zhistory             # The path to the history file.
+HISTSIZE=10000                   # The maximum number of events to save in the internal history.
+SAVEHIST=10000                   # The maximum number of events to save in the history file.
+
+setopt BANG_HIST                 # Treat the '!' character specially during expansion.
+setopt EXTENDED_HISTORY          # Write the history file in the ':start:elapsed;command' format.
+setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
+setopt SHARE_HISTORY             # Share history between all sessions.
+setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate.
+setopt HIST_IGNORE_DUPS          # Do not add command if it is the same as previous
+setopt HIST_FIND_NO_DUPS         # Do not display a previously found event.
+setopt HIST_IGNORE_SPACE         # Do not record an event starting with a space.
+setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history file.
+setopt HIST_NO_FUNCTIONS         # no functions in history
+setopt HIST_VERIFY               # Do not execute immediately upon history expansion.
+
+################
+#  completion  #
+################
+
+setopt COMPLETE_IN_WORD    # Complete from both ends of a word.
+setopt ALWAYS_TO_END       # Move cursor to the end of a completed word.
+setopt PATH_DIRS           # Perform path search even on command names with slashes.
+setopt AUTO_MENU           # Show completion menu on a succesive tab press.
+setopt AUTO_LIST           # Automatically list choices on ambiguous completion.
+setopt AUTO_PARAM_SLASH    # If completed parameter is a directory, add a trailing slash.
+unsetopt MENU_COMPLETE     # Do not autoselect the first completion entry.
+unsetopt FLOW_CONTROL      # Disable start/stop characters in shell editor.
+
+########
+#  cd  #
+########
+
+setopt AUTO_CD              # Auto changes to a directory without typing cd.
+setopt AUTO_PUSHD           # Push the old directory onto the stack on cd.
+setopt PUSHD_IGNORE_DUPS    # Do not store duplicates in the stack.
+setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd.
+setopt PUSHD_TO_HOME        # Push to home directory when no argument is given.
+setopt CDABLE_VARS          # Change directory to a path stored in a variable.
+setopt AUTO_NAME_DIRS       # Auto add variable-stored paths to ~ list.
